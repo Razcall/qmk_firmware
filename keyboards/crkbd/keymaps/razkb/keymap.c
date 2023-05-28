0c8d09bf23e46a4bcc66afebbd188d36df670731
Razcall <77932065+Razcall@users.noreply.github.com>
2023-05-29 00:41:29 +0200

    edit

--- keyboards/crkbd/keymaps/razkb/keymap.c
+++ keyboards/crkbd/keymaps/razkb/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_GAMING] = LAYOUT_split_3x6_3(
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
        CTRLSC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-        T_SFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,  MOSSLH,  KC_ESC,
+        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,  MOSSLH,  KC_ESC,
                                           KC_LALT, GAMING2,  KC_SPC,     KC_ENT,   RAISE, ACCENTS
 
   ),
